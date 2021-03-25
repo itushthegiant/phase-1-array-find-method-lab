63b559cd94fc38aa5361819880ea05c5c5185f87
etamar <etamar.levy@gmail.com>
2021-03-25 18:36:15 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+function superbowlWin(recordArr) {
+    const result = recordArr.find(isWin)
+    return (result ? result.year : result)
+}
+
+
+
+function isWin(gameObj) {
+    return gameObj.result === "W"
+}
